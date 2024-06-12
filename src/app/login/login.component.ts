ae877a79e4d1074fe9108c380e02697a016d814b
azurespear <comeintowind@gmail.com>
2024-06-12 15:57:29 -0700

    login button link to landing page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
+  constructor(private router: Router) { }
   username: string = '';
   password: string = '';
+
   onLoginButtonClick() {
-    console.log('Login button was clicked!');
-    alert('Login button was clicked!');
+    this.router.navigate(['/landing'])
   }
 } 
