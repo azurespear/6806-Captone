e18d77481e99f8fb9249de339678775ffd8a6d30
azurespear <comeintowind@gmail.com>
2024-06-12 15:10:15 -0700

    login page draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
+// Component
+import { AppComponent } from './app.component';
+import { ForumComponent } from './forum/forum.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+
+// Module
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ForumComponent } from './forum/forum.component';
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatRadioModule} from "@angular/material/radio";
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -27,7 +31,9 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatRadioModule
+    MatRadioModule,
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [
     provideClientHydration(),
